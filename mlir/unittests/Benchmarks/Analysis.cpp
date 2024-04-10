78d492619e060cd9c2c63921a9ab5ab8f60755cd
Mogball <jeff@modular.com>
2024-04-10 14:22:51 +0200

    Add benchmark for SymbolTable construction

--- mlir/unittests/Benchmarks/Analysis.cpp
+++ mlir/unittests/Benchmarks/Analysis.cpp
@@ -1,4 +1,4 @@
-//===- Cloning.cpp - Benchmark Op Traveral ---------------------------------- ===//
+//===- Analysis.cpp - Benchmark Attribtues Creation ---------------------- ===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -20,7 +20,8 @@
 using namespace mlir;
 
 namespace {
-class Cloning : public benchmark::Fixture {
+
+class Analysis : public benchmark::Fixture {
 public:
   void SetUp(::benchmark::State &state) final {
     const char *cmd = "bench";
@@ -46,20 +47,15 @@ public:
 };
 } // namespace
 
-BENCHMARK_DEFINE_F(Cloning, cloneOps)(benchmark::State &state) {
+BENCHMARK_DEFINE_F(Analysis, symbolTable)(benchmark::State &state) {
   ctx->loadDialect<TestBenchDialect>();
-  OpBuilder b = OpBuilder::atBlockBegin(moduleOp->getBody());
-  // Create a bunch of ops that have operands.
-  Value operand = moduleOp->getBody()->addArgument(b.getI32Type(), unknownLoc);
-  for (int i = 0; i < state.range(0); ++i) {
-    operand = b.create<PassthroughOp>(unknownLoc, b.getI32Type(), operand);
-  }
-  for (auto _ : state) {
-    OwningOpRef<ModuleOp> moduleClone = moduleOp->clone();
-    benchmark::DoNotOptimize(moduleClone.get());
-  }
+  auto b = OpBuilder::atBlockBegin(moduleOp->getBody());
+  for (int j = 0; j < state.range(0); ++j)
+    b.create<SymbolOp>(unknownLoc, b.getStringAttr("symbol_" + Twine(j)));
+  for (auto _ : state)
+    SymbolTable symtab(*moduleOp);
   state.SetComplexityN(state.range(0));
 }
-BENCHMARK_REGISTER_F(Cloning, cloneOps)
+BENCHMARK_REGISTER_F(Analysis, symbolTable)
     ->Ranges({{10, 10 * 1000 * 1000}})
     ->Complexity(benchmark::oN);
